18c4e66e35ad40a1661c78c92f132445a64f7cb2
RandomGuy090 <kubamaciej2003@gmail.com>
2021-01-24 23:23:54 +0100

    code update v2

--- imageEditor.py
+++ imageEditor.py
@@ -39,12 +39,7 @@ class Application(tk.Frame):
 		self.canvas.lastMoves = list()
 		self.canvas.undoList = []
 
-		self.master.pencilColor = None
-		self.master.rectangleColor = None
-		self.master.rectangle_frameColor = None
-		self.master.color_picker = None
-		self.master.ovalColor = None
-		self.master.ovalFilledColor = None
+		
 		self.master.button1 = None
 		self.master.shift = False
 
@@ -128,14 +123,15 @@ class Application(tk.Frame):
 
 	def key_pressed(self, event):
 		x, y = event.x, event.y 
-		print(f"SHIFT: {self.master.shift}")
+		
 		global draw_Item, draw_Item_list
 			
-		print(draw_Item)
-		print(draw_Item)
-		print(draw_Item)
+		
+		
+		if draw_Item == None:
+			return
 
-		# if self.master.pencilColor == "red":
+
 		if draw_Item == draw_Item_list[0]:
 
 			Draw(self.master, self.canvas).line( x, y)
@@ -162,24 +158,25 @@ class Application(tk.Frame):
 		global draw_Item, draw_Item_list
 		self.master.shift = True
 		x, y = event.x, event.y 
-		if self.master.pencilColor == "red":
+		if draw_Item == draw_Item_list[0]:
+
 			Draw(self.master, self.canvas).line( x, y)
 
-		elif self.master.rectangleColor == "red":
+		elif draw_Item == draw_Item_list[1]:
 			Draw(self.master, self.canvas).rectangle( x, y)
 		
-		elif self.master.rectangle_frameColor == "red":
+		elif draw_Item == draw_Item_list[2]:
 			Draw(self.master, self.canvas).rectangle_frame( x, y)
 
-		elif self.master.ovalColor == "red":
+		elif draw_Item == draw_Item_list[3]:
 			Draw(self.master, self.canvas).oval( x, y)
 
-		elif self.master.ovalFilledColor == "red":
+		elif draw_Item == draw_Item_list[4]:
 			Draw(self.master, self.canvas,self.imagePath).ovalFilled(x, y)
-			
-		elif self.master.color_picker == "red":
+	
+		elif draw_Item == draw_Item_list[5]:
 			Draw(self.master, self.canvas,self.imagePath).get_pixel_val( x, y)
-			# self.master.button1.configure(bg = self.canvas.lineColor)
+
 			self.master.button1.configure(bg = self.canvas.lineColor)
 		
 
@@ -245,7 +242,7 @@ class Draw(Application):
 		self.master = master
 		self.canvas = canvas
 		self.imagePath = imagePath
-		print(f"shift: {self.master.shift}")
+		
 
 	def line(self, x, y):
 		self.y = y
@@ -325,11 +322,11 @@ class Draw(Application):
 			del self.canvas.undoList[-1:]
 
 
+	
 	def motion(self, event):
 		global draw_Item, draw_Item_list
+		
 		#draw line
-
-		# if self.canvas.keyDraw and self.master.pencilColor == "red" :
 		if self.canvas.keyDraw and draw_Item == draw_Item_list[0]:
 			
 			x, y = event.x, event.y
@@ -341,27 +338,13 @@ class Draw(Application):
 			self.canvas.lastMoves.append(self.master.line)
 
 		#draw filled rectangle		
-		# elif self.canvas.keyDraw and self.master.rectangleColor == "red":
 		elif self.canvas.keyDraw and draw_Item == draw_Item_list[1]:
 			self.canvas.delete(self.master.rect)
-			if self.master.xrec ==  None:
-				self.master.xrec =  event.x
-				self.master.yrec =   event.y
+			
+			self.master.xrec, self.master.yrec = self.set_xrec(event.x, event.y)
 
-			x, y = event.x, event.y   
-			if self.master.shift:
-				if x > self.master.xrec and y < self.master.yrec:
-					y = (x - self.master.xrec) - self.master.yrec
-					y = -1 * y
+			x, y = self.make_shifted(event.x, event.y)   
 				
-				elif x < self.master.xrec and y < self.master.yrec:
-					y = (x - self.master.xrec) + self.master.yrec
-				elif x < self.master.xrec and y > self.master.yrec:
-					x = (y - self.master.yrec) - self.master.xrec
-					x = x *-1
-				elif x > self.master.xrec and y > self.master.yrec:
-					x = (y - self.master.yrec) + self.master.xrec 
-								
 			self.master.rect = self.canvas.create_rectangle(self.master.xrec, \
 				self.master.yrec,x, y, fill=self.canvas.lineColor,\
 				outline=self.canvas.lineColor, width=self.canvas.lineWidth,)
@@ -372,24 +355,9 @@ class Draw(Application):
 		elif self.canvas.keyDraw and draw_Item == draw_Item_list[2]:
 			
 			self.canvas.delete(self.master.rect)
-			if self.master.xrec ==  None:
-					self.master.xrec =  event.x
-					self.master.yrec =   event.y
+			self.master.xrec, self.master.yrec = self.set_xrec(event.x, event.y)
 
-			x, y = event.x, event.y
-			if self.master.shift:
-				if x > self.master.xrec and y < self.master.yrec:
-					y = (x - self.master.xrec) - self.master.yrec
-					y = -1 * y
-				
-				elif x < self.master.xrec and y < self.master.yrec:
-					y = (x - self.master.xrec) + self.master.yrec
-				elif x < self.master.xrec and y > self.master.yrec:
-					x = (y - self.master.yrec) - self.master.xrec
-					x = x *-1
-				elif x > self.master.xrec and y > self.master.yrec:
-					x = (y - self.master.yrec) + self.master.xrec
-			
+			x, y = self.make_shifted(event.x, event.y)   
 
 			self.master.rect = self.canvas.create_rectangle(self.master.xrec, \
 				self.master.yrec,x, y, outline=self.canvas.lineColor, 
@@ -404,24 +372,8 @@ class Draw(Application):
 				self.canvas.delete(self.master.oval)
 			except:
 				pass
-			if self.master.xrec ==  None:
-					self.master.xrec =  event.x
-					self.master.yrec =   event.y
-
-			x, y = event.x, event.y
-			if self.master.shift:
-				if x > self.master.xrec and y < self.master.yrec:
-					y = (x - self.master.xrec) - self.master.yrec
-					y = -1 * y
-				
-				elif x < self.master.xrec and y < self.master.yrec:
-					y = (x - self.master.xrec) + self.master.yrec
-				elif x < self.master.xrec and y > self.master.yrec:
-					x = (y - self.master.yrec) - self.master.xrec
-					x = x *-1
-				elif x > self.master.xrec and y > self.master.yrec:
-					x = (y - self.master.yrec) + self.master.xrec
-	
+			self.master.xrec, self.master.yrec = self.set_xrec(event.x, event.y)
+			x, y = self.make_shifted(event.x, event.y)   
 						
 			self.master.oval = self.canvas.create_oval(self.master.xrec, \
 				self.master.yrec,x, y, outline=self.canvas.lineColor, 
@@ -437,33 +389,40 @@ class Draw(Application):
 				self.canvas.delete(self.master.ovalFilled)
 			except:
 				pass
-			if self.master.xrec ==  None:
-					self.master.xrec =  event.x
-					self.master.yrec =   event.y
 
-			x, y = event.x, event.y
-			if self.master.shift:
-				if x > self.master.xrec and y < self.master.yrec:
-					y = (x - self.master.xrec) - self.master.yrec
-					y = -1 * y
-				
-				elif x < self.master.xrec and y < self.master.yrec:
-					y = (x - self.master.xrec) + self.master.yrec
-				elif x < self.master.xrec and y > self.master.yrec:
-					x = (y - self.master.yrec) - self.master.xrec
-					x = x *-1
-				elif x > self.master.xrec and y > self.master.yrec:
-					x = (y - self.master.yrec) + self.master.xrec
+			self.master.xrec, self.master.yrec = self.set_xrec(event.x, event.y)
+
+			x, y = self.make_shifted(event.x, event.y)   
 		
-
-						
 			self.master.ovalFilled = self.canvas.create_oval(self.master.xrec, \
 				self.master.yrec,x, y, outline=self.canvas.lineColor, 
 				width=self.canvas.lineWidth, fill=self.canvas.lineColor)
 
 			self.x ,self.y = x, y
-			
 			self.canvas.lastMoves.append(self.master.ovalFilled)
+
+	def make_shifted(self, x, y):
+		if self.master.shift:
+			if x > self.master.xrec and y < self.master.yrec:
+				y = (x - self.master.xrec) - self.master.yrec
+				y = -1 * y
+			
+			elif x < self.master.xrec and y < self.master.yrec:
+				y = (x - self.master.xrec) + self.master.yrec
+			elif x < self.master.xrec and y > self.master.yrec:
+				x = (y - self.master.yrec) - self.master.xrec
+				x = x *-1
+			elif x > self.master.xrec and y > self.master.yrec:
+				x = (y - self.master.yrec) + self.master.xrec
+		return x, y
+
+	def set_xrec(self,x, y):
+		if self.master.xrec == None:
+			xrec = x
+			yrec = y
+			return xrec, yrec
+		return self.master.xrec, self.master.yrec
+
 
 
 class Sidebar(Application):
@@ -527,7 +486,6 @@ class Sidebar(Application):
 		global buttons_list
 		buttons_list["pencil"] = self.pencil
 		
-
 	def rectangle(self):
 
 		self.rectangle_icon = tk.PhotoImage(width=1, height=1)
@@ -553,7 +511,6 @@ class Sidebar(Application):
 
 		global buttons_list
 		buttons_list["rectangle_frame"] = self.rectangle_frame
-
 
 	def oval(self):
 		# unicode filled circle
@@ -602,54 +559,11 @@ class Sidebar(Application):
 		self.master.button1.configure(bg = self.canvas.lineColor)
 
 	
-	def set_config_buttons(self, pencil=None, rectangle=None,
-		rectangle_frame=None, color_picker=None, oval=None, ovalFilled=None):
+	def set_config_buttons(self):
 		global buttons_list, draw_Item
-		print(f"drwa item: {draw_Item}")
-
-		self.rectangle_frame.configure(bg="#f9ffbd")
-		self.rectangle_frame.configure(fg="red")	
-		self.pencil.configure(bg="#f9ffbd")
-		self.pencil.configure(fg="red")
-		self.rectangle.configure(bg="#f9ffbd")
-		self.rectangle.configure(fg="red")
-		self.color_picker.configure(bg="#f9ffbd")
-		self.color_picker.configure(fg="red")
-		self.oval.configure(bg="#f9ffbd")
-		self.oval.configure(fg="red")
-		self.ovalFilled.configure(bg="#f9ffbd")
-		self.ovalFilled.configure(fg="red")
-
-		# if rectangle_frame == None:
-		# 	self.rectangle_frame.configure(bg="white")
-		# 	self.rectangle_frame.configure(fg="black")
-
-		# if pencil == None:
-		# 	self.pencil.configure(bg="white")
-		# 	self.pencil.configure(fg="black")
-
-		# if rectangle == None:
-		# 	self.rectangle.configure(bg="white")
-		# 	self.rectangle.configure(fg="black")
-
-		# if color_picker == None:
-		# 	self.color_picker.configure(bg="white")
-		# 	self.color_picker.configure(fg="black")
-
-		# if oval == None:
-		# 	self.oval.configure(bg="white")
-		# 	self.oval.configure(fg="black")
-
-		# if ovalFilled == None:
-		# 	self.ovalFilled.configure(bg="white")
-		# 	self.ovalFilled.configure(fg="black")
-
-			
-
-		print("DICT")
+					
 		for elem in buttons_list:
-			print(f"{elem} : {buttons_list[elem]}" )
-			buttons_list[elem].configure(bg="#00ff00")
+			# buttons_list[elem].configure(bg="#00ff00")
 			if draw_Item == elem:
 				buttons_list[elem].configure(bg="#f9ffbd")
 				buttons_list[elem].configure(fg="red")
@@ -657,109 +571,75 @@ class Sidebar(Application):
 				buttons_list[elem].configure(bg="white")
 				buttons_list[elem].configure(fg="black")
 
+	def set_unclicked(self, Id):
+		global buttons_list
+		buttons_list[Id].configure(bg="white")
+		buttons_list[Id].configure(fg="black")
 
-
-		self.master.pencilColor = self.pencil["fg"]
-		self.master.rectangleColor = self.rectangle["fg"]
-		self.master.rectangle_frameColor = self.rectangle_frame["fg"]
-		self.master.color_picker = self.color_picker["fg"]
-		self.master.ovalColor = self.oval["fg"]
-		self.master.ovalFilledColor = self.ovalFilled["fg"]
-		print("")
-
-
-			
 	def set_pencil(self):
 		global draw_Item, draw_Item_list
 		
 		if draw_Item == draw_Item_list[0]:
 			draw_Item = None
+			self.set_unclicked(draw_Item_list[0])
+			
+
 		else:
 			draw_Item = draw_Item_list[0]
 			self.set_config_buttons()
-
-
-		print(f"draw item {draw_Item}")
-		print("PENCIL SET")
-		print("PENCIL SET")
-
-		# if self.pencil["fg"] == "black":		
-		# 	
-		# 	self.master.rectangleColor
-			
 
 	def set_rectangle(self):
 		
 		global draw_Item, draw_Item_list
 		if draw_Item == draw_Item_list[1]:
 			draw_Item = None
+			self.set_unclicked(draw_Item_list[1])
+			
 		else:
 			draw_Item = draw_Item_list[1]
 			self.set_config_buttons()
-
-		# if self.rectangle["fg"] == "black":		
-		# 	self.set_config_buttons()
-		# 	global draw_Item, draw_Item_list
-		# 	draw_Item = draw_Item_list[1]
-
-		print("rectangle SET")
-		print("rectangle SET")
 	
 	def set_rectangle_frame(self):
 
 		global draw_Item, draw_Item_list
 		if draw_Item == draw_Item_list[2]:
 			draw_Item = None
+			self.set_unclicked(draw_Item_list[2])
 		else:
 			draw_Item = draw_Item_list[2]
 			self.set_config_buttons()
-
-		# if self.rectangle_frame["fg"] == "black":		
-		# 	self.set_config_buttons()
-		# 	global draw_Item, draw_Item_list
-		# 	draw_Item = draw_Item_list[2]
-
-		print("rectangle_frame SET")
 
 
 	def set_oval(self):
 		global draw_Item, draw_Item_list		
 		if draw_Item == draw_Item_list[3]:
 			draw_Item = None
+			self.set_unclicked(draw_Item_list[3])
 		else:
 			draw_Item = draw_Item_list[3]
 			self.set_config_buttons()
-		# if self.oval["fg"] == "black":		
-		# 	self.set_config_buttons()
-		# 	global draw_Item, draw_Item_list
-		# 	draw_Item = draw_Item_list[3]
 
 	def set_oval_filled(self):
 		global draw_Item, draw_Item_list
 		if draw_Item == draw_Item_list[4]:
 			draw_Item = None
+			self.set_unclicked(draw_Item_list[4])
 		else:
 			draw_Item = draw_Item_list[4]
 			self.set_config_buttons()
 
-		# if self.ovalFilled["fg"] == "black":		
-		# 	self.set_config_buttons()
-		# 	print("OVAL FILLED")
-		# 	global draw_Item, draw_Item_list
-		# 	draw_Item = draw_Item_list[4]
+		
 
 	def set_color_picker(self):
 		global draw_Item, draw_Item_list
 		if draw_Item == draw_Item_list[5]:
 			draw_Item = None
+			self.set_unclicked(draw_Item_list[5])
 		else:
 			draw_Item = draw_Item_list[5]
 			self.set_config_buttons()
 
-		# if self.color_picker["fg"] == "black":		
-		# 	self.set_config_buttons()			
-		# 	global draw_Item, draw_Item_list
-		# 	draw_Item = draw_Item_list[5]
+	
 
 def show_help():
 	print("""./imageEditor.py
